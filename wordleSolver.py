0c526b7119dd52c163a4e90643c21fb4f56805c8
rodri270 <rodri270@go.stockton.edu>
2022-01-27 10:21:09 -0500

    Added code for solution

--- wordleSolver.py
+++ wordleSolver.py
@@ -1,9 +1,17 @@
 from selenium import webdriver
 import json
+
+
 driver = webdriver.Firefox() 
+
 url='https://www.powerlanguage.co.uk/wordle/'
+
 driver.get(url)
+
 script="return localStorage;"
+
 result = driver.execute_script(script)
+
 solution = json.loads(result["gameState"])
+
 print(solution["solution"])
