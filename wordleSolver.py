b9d3a6fe3984c739a36f26f1ccce945d97955c88
rodri270 <rodri270@go.stockton.edu>
2022-01-27 18:58:06 -0500

    added for look to better understand data

--- wordleSolver.py
+++ wordleSolver.py
@@ -1,22 +1,18 @@
 from selenium import webdriver
 import json
-
 #for this part to work you need to have the mozzila geckodrivers downloaded which can be found on their github
 #Once downloaded you need to move the file to the same folder that has the python exe 
 #if on macos you need to do the following things 
-'''
-    control-click on the geckodriver binary but keep the mouse button/touch pad pressed...
+'''control-click on the geckodriver binary but keep the mouse button/touch pad pressed...
     remove finger from control key and press on option key...
     you will see the menu change...
     then select the "open" menu item with the mouse/touchpad and it will open in the terminal.
     close the terminal and you are done. You have given macOS permission to run that binary.
-    Note: this works for all binaries Catalina rejects so this works for chromedriver for Chrome as well
-
-'''
+    Note: this works for all binaries Catalina rejects so this works for chromedriver for Chrome as well'''
 #assings webdriver.firefox to driver. This Allows you to pick which browser you would like to use with selenium 
 driver = webdriver.Firefox() 
 
-#Urlf of wordle 
+#Url of wordle 
 url='https://www.powerlanguage.co.uk/wordle/'
 
 #Tells the driver to get the content of wordle
@@ -27,6 +23,11 @@ script="return localStorage;"
 
 #calling the execute_script funtion so we can inject javascript into the website and saves output to results 
 result = driver.execute_script(script)
+
+#testing code to better understand how the data was being brought in 
+'''result_Items= result.items()
+for item in result_Items:
+    print(item)'''
 
 #because of how the data is retrieved python struggles to parse throught it. We use json.loads and tells it to select the gameState key
 solution = json.loads(result["gameState"])
